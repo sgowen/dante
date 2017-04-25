b30a164163246278fd8e3b9d07d09609de679806
Stephen Gowen <dev.sgowen@gmail.com>
2017-04-25 15:55:24 -0500

    Framework Update

--- src/core/framework/graphics/direct3d/Direct3DManager.cpp
+++ src/core/framework/graphics/direct3d/Direct3DManager.cpp
@@ -495,11 +495,13 @@ m_iNumFramebuffers(-1),
 m_isWindowsMobile(false)
 {
 	// Hide Constructor for Singleton
-#if !defined(WINAPI_FAMILY) || WINAPI_FAMILY == WINAPI_FAMILY_DESKTOP_APP || WINAPI_FAMILY == WINAPI_FAMILY_PC_APP
+#if !defined(WINAPI_FAMILY) || WINAPI_FAMILY == WINAPI_FAMILY_DESKTOP_APP
 	m_isWindowsMobile = false;
 #elif defined (WINAPI_FAMILY_SYSTEM)
 	Windows::System::Profile::AnalyticsVersionInfo^ api = Windows::System::Profile::AnalyticsInfo::VersionInfo;
 	m_isWindowsMobile = api->DeviceFamily->Equals("Windows.Mobile");
+#elif WINAPI_FAMILY == WINAPI_FAMILY_PC_APP
+	m_isWindowsMobile = false;
 #else
 	m_isWindowsMobile = true;
 #endif
