aed6ac2839b6f65d7c90ba84fa99e97fb487fe0f
Stephen Gowen <dev.sgowen@gmail.com>
2017-05-03 17:28:40 -0500

    Getting things building again

---
+++ src/core/game/tools/encrypt_textures_and_shaders.sh
@@ -0,0 +1,12 @@
+cd ../../framework/graphics/opengl/shader
+
+../../../tools/XOREncryptDecrypt shader_001_frag.fsh shader_001_frag.ngs
+../../../tools/XOREncryptDecrypt shader_002_frag.fsh shader_002_frag.ngs
+../../../tools/XOREncryptDecrypt shader_003_frag.fsh shader_003_frag.ngs
+../../../tools/XOREncryptDecrypt shader_001_vert.vsh shader_001_vert.ngs
+../../../tools/XOREncryptDecrypt shader_002_vert.vsh shader_002_vert.ngs
+../../../tools/XOREncryptDecrypt shader_003_vert.vsh shader_003_vert.ngs
+
+cd ../../../../../../assets/textures/
+
+../../src/core/framework/tools/XOREncryptDecrypt texture_001.png texture_001.ngt
