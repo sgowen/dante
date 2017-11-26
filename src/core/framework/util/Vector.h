deab2250852ae0a5c32974d10b41ad9aade833ba
Stephen Gowen <dev.sgowen@gmail.com>
2017-11-26 11:46:02 -0500

    Making Vector smarter.

--- src/core/framework/util/Vector.h
+++ src/core/framework/util/Vector.h
@@ -26,16 +26,51 @@ namespace NoctisGames
             // Empty
         }
         
-        Vector(const Vector& inArray) : _size(inArray._size), _capacity(inArray._capacity), _buffer(NULL)
+        Vector(const Vector& inVector) : _size(inVector._size), _capacity(inVector._capacity), _buffer(NULL)
         {
             if (_capacity > 0)
             {
                 _buffer = allocate(_capacity);
                 for (size_t i = 0; i < _size; ++i)
                 {
-                    construct(_buffer + i, inArray._buffer[i]);
+                    construct(_buffer + i, inVector._buffer[i]);
                 }
             }
+        }
+        
+        Vector(Vector& inVector) : _size(inVector._size), _capacity(inVector._capacity), _buffer(NULL)
+        {
+            if (_capacity > 0)
+            {
+                _buffer = allocate(_capacity);
+                for (size_t i = 0; i < _size; ++i)
+                {
+                    construct(_buffer + i, inVector._buffer[i]);
+                }
+            }
+        }
+        
+        Vector& operator=(Vector& inVector)
+        {
+            if (this != &inVector)
+            {
+                clear();
+                deallocate(_buffer);
+                
+                _size = inVector._size;
+                _capacity = inVector._capacity;
+                
+                if (_capacity > 0)
+                {
+                    _buffer = allocate(_capacity);
+                    for (size_t i = 0; i < _size; ++i)
+                    {
+                        construct(_buffer + i, inVector._buffer[i]);
+                    }
+                }
+            }
+            
+            return *this;
         }
         
         ~Vector()
@@ -55,6 +90,19 @@ namespace NoctisGames
             }
             
             return false;
+        }
+        
+        int indexOf(const T& inValue)
+        {
+            for (size_t i = 0; i < _size; ++i)
+            {
+                if (_buffer[i] == inValue)
+                {
+                    return static_cast<int>(i);
+                }
+            }
+            
+            return -1;
         }
         
         void push_back(const T& inValue)
@@ -142,6 +190,29 @@ namespace NoctisGames
         T* end()
         {
             return &_buffer[_size];
+        }
+        
+        friend bool operator==(Vector<T>& lhs, Vector<T>& rhs)
+        {
+            if (lhs.size() != rhs.size())
+            {
+                return false;
+            }
+            
+            for (int i = 0, n = static_cast<int>(lhs.size()); i < n; ++i)
+            {
+                if (lhs[i] != rhs[i])
+                {
+                    return false;
+                }
+            }
+            
+            return true;
+        }
+        
+        friend bool operator!=(Vector<T>& lhs, Vector<T>& rhs)
+        {
+            return !(lhs == rhs);
         }
         
     private:
