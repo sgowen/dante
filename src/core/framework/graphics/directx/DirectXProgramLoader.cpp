772836f1041aceb14e7cb20dc1a3438f8d28b075
Stephen Gowen <dev.sgowen@gmail.com>
2017-12-18 20:45:02 -0500

    Hmmm.. will it just work? Find out next time on DRAGON BALL Z~~~!!!

--- src/core/framework/graphics/directx/DirectXProgramLoader.cpp
+++ src/core/framework/graphics/directx/DirectXProgramLoader.cpp
@@ -1,5 +1,5 @@
 //
-//  DirectXProgram.cpp
+//  DirectXProgramLoader.cpp
 //  noctisgames-framework
 //
 //  Created by Stephen Gowen on 2/17/17.
@@ -8,16 +8,30 @@
 
 #include "pch.h"
 
-#include "framework/graphics/directx/DirectXProgram.h"
+#include "framework/graphics/directx/DirectXProgramLoader.h"
 
-#include "framework/graphics/directx/DirectXRendererHelper.h"
-#include "framework/util/StringUtil.h"
+#include "framework/graphics/portable/ShaderProgramWrapper.h"
+#include "framework/graphics/portable/NGShaderVarInput.h"
+#include "framework/graphics/portable/NGShaderUniformInput.h"
+
 #include "framework/file/portable/AssetDataHandler.h"
 #include "framework/file/portable/FileData.h"
+#include "framework/util/StringUtil.h"
+#include <framework/graphics/directx/DirectXRendererHelper.h>
 
 #include "PlatformHelpers.h"
 
-DirectXProgram::DirectXProgram(DirectXRendererHelper* inRendererHelper, const char* vertexShaderName, const char* fragmentShaderName, const D3D11_INPUT_ELEMENT_DESC* inputElementDescs, UINT numElements) : _rendererHelper(inRendererHelper), _d3dDevice(DirectXRendererHelper::getD3dDevice()), _d3dContext(DirectXRendererHelper::getD3dContext())
+DirectXProgramLoader::DirectXProgramLoader() : ShaderProgramLoader()
+{
+    // Empty
+}
+
+DirectXProgramLoader::~DirectXProgramLoader()
+{
+    // Empty
+}
+
+ShaderProgramWrapper* DirectXProgramLoader::loadShaderProgram(const char* vertexShaderName, const char* fragmentShaderName, std::vector<NGShaderUniformInput*>& uniforms, std::vector<NGShaderVarInput*>& inputLayout)
 {
     std::string s1("data\\shaders\\");
 	s1 += std::string(vertexShaderName);
@@ -31,22 +45,44 @@ DirectXProgram::DirectXProgram(DirectXRendererHelper* inRendererHelper, const ch
     unsigned char* vertex_shader_source_output = (unsigned char*) malloc(vertex_shader_source.data_length);
     StringUtil::encryptDecrypt((unsigned char*)vertex_shader_source.data, vertex_shader_source_output, vertex_shader_source.data_length);
     
+    ID3D11VertexShader* pVertexShader;
+    ID3D11InputLayout* pInputLayout;
+    ID3D11PixelShader* pPixelShader;
+    
+    ID3D11Device* d3dDevice = DirectXRendererHelper::getD3dDevice();
+    
 	DirectX::ThrowIfFailed(
-		_d3dDevice->CreateVertexShader(
+		d3dDevice->CreateVertexShader(
 			vertex_shader_source_output,
 			vertex_shader_source.data_length,
 			NULL,
-			&_vertexShader
+			&pVertexShader
 		)
 	);
+    
+    const D3D11_INPUT_ELEMENT_DESC inputElementDescs[3] = {
+        { "a_Position", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0, 0, D3D11_INPUT_PER_VERTEX_DATA, 0 },
+        { "a_Color", 0, DXGI_FORMAT_R32G32B32A32_FLOAT, 0, 12, D3D11_INPUT_PER_VERTEX_DATA, 0 },
+        { "a_TexCoord", 0, DXGI_FORMAT_R32G32_FLOAT, 0, 28, D3D11_INPUT_PER_VERTEX_DATA, 0 }
+    };
+    
+    std::vector<D3D11_INPUT_ELEMENT_DESC> inputElementDescs;
+    inputElementDescs.reserve(inputLayout.size());
+    
+    for (std::vector<NGShaderVarInput*>::iterator i = inputLayout.begin(); i != inputLayout.end(); ++i)
+    {
+        NGShaderVarInput* svi = (*i);
+        
+        inputElementDescs.push_back(svi->_attribute);
+    }
 
 	DirectX::ThrowIfFailed(
-		_d3dDevice->CreateInputLayout(
+		d3dDevice->CreateInputLayout(
             inputElementDescs,
-			numElements,
+			inputElementDescs.size(),
 			vertex_shader_source_output,
 			vertex_shader_source.data_length,
-			&_inputLayout
+			&pInputLayout
 		)
 	);
 
@@ -55,11 +91,11 @@ DirectXProgram::DirectXProgram(DirectXRendererHelper* inRendererHelper, const ch
     StringUtil::encryptDecrypt((unsigned char*)fragment_shader_source.data, fragment_shader_source_output, fragment_shader_source.data_length);
     
 	DirectX::ThrowIfFailed(
-		_d3dDevice->CreatePixelShader(
+		d3dDevice->CreatePixelShader(
 			fragment_shader_source_output,
 			fragment_shader_source.data_length,
 			NULL,
-			&_pixelShader
+			&pPixelShader
 		)
 	);
     
@@ -68,11 +104,6 @@ DirectXProgram::DirectXProgram(DirectXRendererHelper* inRendererHelper, const ch
     
     free((void *)vertex_shader_source_output);
     free((void *)fragment_shader_source_output);
-}
-
-DirectXProgram::~DirectXProgram()
-{
-	_vertexShader.Reset();
-	_inputLayout.Reset();
-	_pixelShader.Reset();
+    
+    return new ShaderProgramWrapper(pVertexShader, pInputLayout, pPixelShader);
 }
---
+++ src/core/framework/graphics/directx/DirectXProgramLoader.h
@@ -0,0 +1,26 @@
+//
+//  DirectXProgramLoader.h
+//  noctisgames-framework
+//
+//  Created by Stephen Gowen on 2/17/17.
+//  Copyright (c) 2017 Noctis Games. All rights reserved.
+//
+
+#ifndef __noctisgames__DirectXProgramLoader__
+#define __noctisgames__DirectXProgramLoader__
+
+#include <framework/graphics/portable/ShaderProgramLoader.h>
+
+#include "framework/graphics/portable/NGGraphics.h"
+
+class DirectXProgramLoader : public ShaderProgramLoader
+{
+public:
+    DirectXProgramLoader();
+    
+    virtual DirectXProgramLoader();
+    
+    virtual ShaderProgramWrapper* loadShaderProgram(const char* vertexShaderName, const char* fragmentShaderName, std::vector<NGShaderUniformInput*>& uniforms, std::vector<NGShaderVarInput*>& inputLayout);
+};
+
+#endif /* defined(__noctisgames__DirectXProgramLoader__) */
