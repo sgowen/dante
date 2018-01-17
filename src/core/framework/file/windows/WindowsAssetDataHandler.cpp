3405ab36c56e8315bf8e2d1376a53f2f48f27e34
Stephen Gowen <dev.sgowen@gmail.com>
2018-01-17 17:32:58 -0500

    Fixing Windows Asset Data Handler.

--- src/core/framework/file/windows/WindowsAssetDataHandler.cpp
+++ src/core/framework/file/windows/WindowsAssetDataHandler.cpp
@@ -17,107 +17,75 @@
 
 WindowsAssetDataHandler* WindowsAssetDataHandler::getInstance()
 {
-    static WindowsAssetDataHandler instance = WindowsAssetDataHandler();
-    return &instance;
+	static WindowsAssetDataHandler instance = WindowsAssetDataHandler();
+	return &instance;
 }
 
 FileData WindowsAssetDataHandler::getAssetData(const char* relativePath, bool isText)
 {
-    assert(relativePath != NULL);
-    
-    if (isText)
-    {
-        FILE *stream;
-        assert(fopen_s(&stream, relativePath, "r") == 0);
-        assert(stream != NULL);
-        
-		// seek to end of file
-		fseek(stream, 0, SEEK_END);
+	assert(relativePath != NULL);
 
-		// get current file position which is end from seek
-		size_t size = ftell(stream);
+	wchar_t* name = new wchar_t[4096];
+	MultiByteToWideChar(CP_ACP, 0, relativePath, -1, name, 4096);
 
-		std::string* rawDataP = new std::string();
-		std::string& rawData = *rawDataP;
+	std::ifstream inFile(name, std::ios::in | std::ios::binary | std::ios::ate);
 
-		// allocate string space and set length
-		rawData.resize(size);
+	if (!inFile)
+	{
+		wchar_t moduleName[_MAX_PATH];
 
-		// go back to beginning of file for read
-		rewind(stream);
+		assert(GetModuleFileNameW(NULL, moduleName, _MAX_PATH));
 
-		// read 1*size bytes from sfile into ss
-		fread(&rawData[0], 1, size, stream);
+		wchar_t drive[_MAX_DRIVE];
+		wchar_t path[_MAX_PATH];
 
-		// close the file
-		fclose(stream);
-        
-        return FileData(size, (void*)rawData.c_str(), rawDataP, 0);
-    }
-    else
-    {
-        wchar_t* name = new wchar_t[4096];
-        MultiByteToWideChar(CP_ACP, 0, relativePath, -1, name, 4096);
-        
-        std::ifstream inFile(name, std::ios::in | std::ios::binary | std::ios::ate);
-        
-        if (!inFile)
-        {
-            wchar_t moduleName[_MAX_PATH];
-            
-            assert(GetModuleFileNameW(NULL, moduleName, _MAX_PATH));
-            
-            wchar_t drive[_MAX_DRIVE];
-            wchar_t path[_MAX_PATH];
-            
-            assert(!_wsplitpath_s(moduleName, drive, _MAX_DRIVE, path, _MAX_PATH, NULL, 0, NULL, 0));
-            
-            wchar_t filename[_MAX_PATH];
-            assert(!_wmakepath_s(filename, _MAX_PATH, drive, path, name, NULL));
-            
-            inFile.open(filename, std::ios::in | std::ios::binary | std::ios::ate);
-        }
-        
-        assert(inFile);
-        
-        std::streampos len = inFile.tellg();
-        assert(inFile);
-        
-        std::vector<uint8_t>* blob = new std::vector<uint8_t>();
-        blob->resize(size_t(len));
-        
-        inFile.seekg(0, std::ios::beg);
-        assert(inFile);
-        
-        inFile.read(reinterpret_cast<char*>(blob->data()), len);
-        assert(inFile);
-        
-        inFile.close();
-        
-        delete name;
-        
-        return FileData(blob->size(), blob->data(), blob, 1);
-    }
+		assert(!_wsplitpath_s(moduleName, drive, _MAX_DRIVE, path, _MAX_PATH, NULL, 0, NULL, 0));
+
+		wchar_t filename[_MAX_PATH];
+		assert(!_wmakepath_s(filename, _MAX_PATH, drive, path, name, NULL));
+
+		inFile.open(filename, std::ios::in | std::ios::binary | std::ios::ate);
+	}
+
+	assert(inFile);
+
+	std::streampos len = inFile.tellg();
+	assert(inFile);
+
+	std::vector<uint8_t>* blob = new std::vector<uint8_t>();
+	blob->resize(size_t(len));
+
+	inFile.seekg(0, std::ios::beg);
+	assert(inFile);
+
+	inFile.read(reinterpret_cast<char*>(blob->data()), len);
+	assert(inFile);
+
+	inFile.close();
+
+	delete name;
+
+	return FileData(blob->size(), blob->data(), blob, 1);
 }
 
 void WindowsAssetDataHandler::releaseAssetData(const FileData* fileData)
 {
-    assert(fileData != NULL);
+	assert(fileData != NULL);
 	assert(fileData->file_handle != NULL);
 
-    if (fileData->flags > 0)
-    {
-        const std::vector<uint8_t>* handle = static_cast<const std::vector<uint8_t>*>(fileData->file_handle);
-        delete handle;
-    }
-    else
-    {
-        const std::string* handle = static_cast<const std::string*>(fileData->file_handle);
-        delete handle;
-    }
+	if (fileData->flags > 0)
+	{
+		const std::vector<uint8_t>* handle = static_cast<const std::vector<uint8_t>*>(fileData->file_handle);
+		delete handle;
+	}
+	else
+	{
+		const std::string* handle = static_cast<const std::string*>(fileData->file_handle);
+		delete handle;
+	}
 }
 
 WindowsAssetDataHandler::WindowsAssetDataHandler() : AssetDataHandler()
 {
-    // Empty
+	// Empty
 }
